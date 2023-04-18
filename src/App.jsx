e807590c48392fe3421f046e54876ff02eaad86c
STYersonH <102554781+STYersonH@users.noreply.github.com>
2023-04-17 21:00:16 -0500

    adding circle of get Started ->

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
 
 const App = () => {
   return (
     <div className=" bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        {/* Navbar -> <Navbar/>
+            1. Select all word components with ALT
+            2. CTRL + <- and pop < 
+            3. CTRL + -> and pop />  */}
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Stats</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
   );
